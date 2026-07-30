719d8a02fa843c2d05812adb7ea2ac8947f1e537
Muhammed Adhil <adhilnizar86@gmail.com>
2026-07-30 17:02:01 +0530

    refactor(insurance): modularize employee insurance tab components and match UI layout

--- src/components/insurance/employee_insurance_tab.jsx
+++ src/components/insurance/employee_insurance_tab.jsx
@@ -16,7 +16,7 @@ import DependensCovearedCard from "./sections/dependentscoveredcard";
 import InsuranceDocumentCard from "./sections/insurancedocumentcard";
 import ClaimInformationCard from "./sections/claimInformation_card";
 import AuditInformationCard from "./sections/auditInformation_card";
-import GlowButton from "../../ui/glowbutton";
+import GlowButton from "../../components/helpers/glowbutton";
 
 export default function EmployeeInsuranceTab({
   uuid: propUuid,
